91479c17fccf8892b0c4c0688a29d1c342eed75c
Alex Votry <youremail@gmail.com>
2019-06-11 14:11:55 -0700

    breaks with delete. Two components have access to the event-bus and updates independently

--- src/app/event-bus/event-bus.component.ts
+++ src/app/event-bus/event-bus.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { globalEventBus, LESSONS_LIST_AVAILABLE } from './event-bus';
+import { globalEventBus, LESSONS_LIST_AVAILABLE, ADD_NEW_LESSON } from './event-bus';
 import { testLessons } from '../shared/model/test-lessons';
+import { Lesson } from '../shared/model/lesson';
 
 @Component({
   selector: 'event-bus',
@@ -9,11 +10,26 @@ import { testLessons } from '../shared/model/test-lessons';
 })
 export class EventBusComponent implements OnInit {
 
-  constructor() { }
-
+  private lessons: Lesson [] = [];
+  
   ngOnInit() {
     console.log('Top level component broadcasted all lessons ...');
-    globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE, testLessons);
+    this.lessons = testLessons.slice(0);
+    globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE, this.lessons);
+
+    setTimeout(() => {
+      this.lessons.push({
+        id: Math.random(),
+        description: 'New lesson arriving from the backend'
+      });
+      globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE, this.lessons);
+    }, 10000);
+
+  }
+
+
+  addLesson(lessonText: string) {
+    globalEventBus.notifyObservers(ADD_NEW_LESSON, lessonText);
   }
 
 }
--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { globalEventBus, Observer, LESSONS_LIST_AVAILABLE } from '../event-bus/event-bus';
+import { Component, OnInit, DefaultIterableDiffer } from '@angular/core';
+import { globalEventBus, Observer, LESSONS_LIST_AVAILABLE, ADD_NEW_LESSON } from '../event-bus/event-bus';
 import { Lesson } from '../shared/model/lesson';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'lessons-list',
@@ -9,20 +10,37 @@ import { Lesson } from '../shared/model/lesson';
 })
 export class LessonsListComponent implements OnInit, Observer {
 
+  lessons: Lesson[] = [];
+
   constructor() { 
     console.log('lesson list component is registered as observer');
-    console.log('this:', this);
     globalEventBus.registerObserver(LESSONS_LIST_AVAILABLE, this);  
+    globalEventBus.registerObserver(ADD_NEW_LESSON, {
+      notify: lessonText => {
+        this.lessons.push({
+          id: Math.random(),
+          description: lessonText,
+          completed: false
+        })
+      }
+    });
   }
-
-  lessons: Lesson[] = [];
 
   ngOnInit() {
   }
 
   notify(data:Lesson[]) {
     console.log('Lessons list component received data');
-    this.lessons = data;
+    this.lessons = data.slice(0);
+  }
+  toggleLessonsViewed(lesson:Lesson) {
+    console.log('toggling lesson ...');
+    lesson.completed = !lesson.completed;
+  }
+
+  delete(deleted:Lesson) {
+    console.log('delete lesson');
+    _.remove(this.lessons, lesson => lesson.id === deleted.id)
   }
   
 }
